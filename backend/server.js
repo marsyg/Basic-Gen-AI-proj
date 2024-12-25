f5baa77cd5104fb43008a8ea1a37fea21b14c1e1
marsyg <net.maaz@yahoo.com>
2024-12-25 21:25:41 +0530

    uploadImage and storage

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,37 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const {generativeAi} = require("./generativeAi");
+const {generativeAi, GoogleGenerativeAI} = require("@google/generative-ai");
 app.use(cors());
 app.use(express.json());
 const fs = require("fs");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.originalname + '-' + uniqueSuffix);
+    },
+})
+const upload = multer({
+    storage: storage,
+   }).single("image");  
 require("dotenv").config();
-const genAI = new generativeAi(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+let filePath ;
+app.post("/upload",  (req, res) => {
+    upload(req, res, async (err) => {
+        if(err) {
+            console.log(err);
+            return res.status(500).send("Internal Server Error");
+        }
+        filePath = req.file.path;
+    }
+    )
+}
+) 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,63 @@
+import { use } from 'react'
 import { useState } from 'react'
-
+import { useEffect } from 'react'
 
 function App() {
   const [image, setImage] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [selectedFile,setSelectedFile] =useState(null)
+    const [preview, setPreview] = useState(null)
+    useEffect(() => {
+      if (!selectedFile) {
+        setPreview(null)
+        return
+      }
+      const objectUrl = URL.createObjectURL(selectedFile)
+      setPreview(objectUrl)
+      
+      return () => URL.revokeObjectURL(objectUrl)
+    }, [selectedFile])
+
   const uploadImage = async (e) => {
+
+    
+   console.log(e)
+   
     const file = e.target.files[0]
+    console.log(file)
+    if (!file || file.length === 0) {
+      setSelectedFile(null)
+      return
+    }else{
+      setSelectedFile(file)
+    }
+   
     const formData = new FormData()
     formData.append('image', file)
     setLoading(true)
     try {
       const res = await fetch('http://localhost:3000/upload', {
         method: 'POST',
-        body: formData
+        body: formData,
+        responseType: 'blob'
       })
       const data = await res.json()
-      setImage(data.image)
+      setImage(file||data.imageURL)
     } catch (error) {
       console.log(error)
     }
     setLoading(false)
+   
   }
-
+  console.log(selectedFile,"selected file")
+  console.log(preview,"preview")
   return (
     <>
       <div className=''>
-        <img src={URL.createObjectURL(image)} ></img>
+        
         <label htmlFor="image">upload image</label>
-        <input id="image" accept="image*/" type="file" />
+        <input onChange={uploadImage} id="image" name='' accept="image*/" type="file" />
+       {selectedFile &&  <img src={preview} alt="preview" />}
       </div>
     </>
   )
