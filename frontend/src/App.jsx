f4b052008e20fb1c5a8abd857c678b07c8d1ba7c
marsyg <net.maaz@yahoo.com>
2024-12-27 14:14:57 +0530

    analyzeImage frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,109 @@
-import { use } from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [image, setImage] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [selectedFile,setSelectedFile] =useState(null)
-    const [preview, setPreview] = useState(null)
-    useEffect(() => {
-      if (!selectedFile) {
-        setPreview(null)
-        return
-      }
-      const objectUrl = URL.createObjectURL(selectedFile)
-      setPreview(objectUrl)
-      
-      return () => URL.revokeObjectURL(objectUrl)
-    }, [selectedFile])
-
-  const uploadImage = async (e) => {
-
-    
-   console.log(e)
-   
-    const file = e.target.files[0]
-    console.log(file)
-    if (!file || file.length === 0) {
-      setSelectedFile(null)
-      return
-    }else{
-      setSelectedFile(file)
+  const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const [supriseVal, setSupriseVal] = useState("");
+  const [response, setResponse] = useState("");
+  const [imgErorr, setImgError] = useState("");
+  //
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(null);
+      return;
     }
-   
-    const formData = new FormData()
-    formData.append('image', file)
-    setLoading(true)
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
+  //
+  const surpriseOptions = [
+    "Is this a car or a truck?",
+    "Is this a cat or a dog?",
+    "Is this a bird or a plane?",
+  ];
+  //
+  const surprise = () => {
+    let Val =
+      surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]; //randomly select a surprise (math.random selects a random number between 0 and 1 and math.floor rounds it down to the nearest whole number
+    setSupriseVal(Val);
+  };
+  //
+  console.log(supriseVal, "supriseVal");
+  //
+  const analyzeImage = async () => {
+    if (!image) {
+      setImgError("image not found e");
+    }
     try {
-      const res = await fetch('http://localhost:3000/upload', {
-        method: 'POST',
-        body: formData,
-        responseType: 'blob'
-      })
-      const data = await res.json()
-      setImage(file||data.imageURL)
+      const options = {
+        method: "POST",
+        body: supriseVal,
+      };
+      const Response = await fetch("http://localhost:3000/upload", options);
+      const data = await Response.json();
+      setResponse(data);
     } catch (error) {
-      console.log(error)
+      console.error(error);
     }
-    setLoading(false)
-   
-  }
-  console.log(selectedFile,"selected file")
-  console.log(preview,"preview")
+  };
+  //
+  const uploadImage = async (e) => {
+    console.log(e);
+    const file = e.target.files[0];
+    console.log(file);
+    if (!file || file.length === 0) {
+      setSelectedFile(null);
+      return;
+    } else {
+      setSelectedFile(file);
+    }
+    const formData = new FormData();
+    formData.append("image", file);
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:3000/upload", {
+        method: "POST",
+        body: formData,
+        responseType: "blob",
+      });
+      const data = await res.json();
+      setImage(file || data.imageURL);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  console.log(selectedFile, "selected file");
+  console.log(preview, "preview");
   return (
     <>
-      <div className=''>
-        
+      <div className="">
         <label htmlFor="image">upload image</label>
-        <input onChange={uploadImage} id="image" name='' accept="image*/" type="file" />
-       {selectedFile &&  <img src={preview} alt="preview" />}
+        <input
+          onChange={uploadImage}
+          id="image"
+          name=""
+          accept="image*/"
+          type="file"
+        />
+        {selectedFile && <img src={preview} alt="preview" />}
+        <input
+          onChange={(e) => {
+            setSupriseVal(e.target.value);
+          }}
+          type="text"
+          value={supriseVal}
+          placeholder="What do you want to know about the image ..."
+        />
+        <button onClick={surprise}>Surprise</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
