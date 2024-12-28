49bf951125657f52c4a743994f42312cc02e74fa
marsyg <net.maaz@yahoo.com>
2024-12-28 14:25:55 +0530

    update frontend layout for image upload and surprise functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     "Is this a bird or a plane?",
   ];
   //
-  const surprise = () => {
+  const suprise = () => {
     let Val =
       surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]; //randomly select a surprise (math.random selects a random number between 0 and 1 and math.floor rounds it down to the nearest whole number
     setSupriseVal(Val);
@@ -86,26 +86,76 @@ function App() {
   console.log(preview, "preview");
   return (
     <>
-      <div className="">
-        <label htmlFor="image">upload image</label>
-        <input
-          onChange={uploadImage}
-          id="image"
-          name=""
-          accept="image*/"
-          type="file"
-        />
-        {selectedFile && <img src={preview} alt="preview" />}
-        <input
-          onChange={(e) => {
-            setSupriseVal(e.target.value);
-          }}
-          type="text"
-          value={supriseVal}
-          placeholder="What do you want to know about the image ..."
-        />
-        <button onClick={surprise}>Surprise</button>
-        <button onClick={analyzeImage}>Generate</button>
+      <div className="flex self-center justify-center w-full h-screen bg-gray-900">
+        <div className="hidden">
+          <input
+            onChange={uploadImage}
+            id="image"
+            placeholder="Click to upload image"
+            accept="image*/"
+            type="file"
+          />
+          {selectedFile && <img src={preview} alt="preview" />}
+        </div>
+
+        <div className="flex flex-col w-3/5 h-28 px-1 border-spacing-1 border-slate-200 items-center rounded-3xl fixed bottom-7    bg-slate-700 justify-center">
+          <div className="w-full ">
+            <input
+              onChange={(e) => {
+                setSupriseVal(e.target.value);
+              }}
+              className="border-2 w-full h-14 my-1 rounded-2xl py-1  bg-slate-300 border-stone-500"
+              type="text"
+              value={supriseVal}
+              placeholder="What do you want to know about the image ..."
+            />
+          </div>
+          <div className="flex flex-col w-full  ">
+            <div className="flex w-full justify-between ">
+              <div className="flex flex-row">
+                <input
+                  onChange={uploadImage}
+                  id="image"
+                  placeholder="Click to upload image"
+                  accept="image*/"
+                  type="file"
+                  className="hidden"
+                />
+                <label className="" htmlFor="image">
+                  <div className="p-2  rounded-xl mx-1 text-slate-300 bg-slate-600 hover:bg-slate-500 cursor-pointer">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                      />
+                    </svg>
+                  </div>
+                </label>
+                <button
+                  className="p-2 rounded-xl text-slate-300 bg-slate-600  hover:bg-slate-500"
+                  onClick={suprise}
+                >
+                  Suprise Me
+                </button>
+              </div>
+
+              <button
+                className="p-2 rounded-xl mx-1 text-slate-300 bg-slate-600 hover:bg-slate-500"
+                onClick={analyzeImage}
+              >
+                Generate
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
