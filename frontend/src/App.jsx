dcb17a4b69b05762726af51a0f41ff53a890d23c
marsyg <net.maaz@yahoo.com>
2024-12-29 23:09:33 +0530

    introducing speech recognition functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,15 @@ function App() {
   const [supriseVal, setSupriseVal] = useState("");
   const [response, setResponse] = useState("");
   const [imgErorr, setImgError] = useState("");
+  const handleOnRecord = () => {
+    const speechRecognition = window.webkitSpeechRecognition|| window.SpeechRecognition;
+    const recognition = new speechRecognition();
+    recognition.start();
+    recognition.onresult = (e) => {
+     
+      console.log(e);
+     }
+  }
   //
   useEffect(() => {
     if (!selectedFile) {
@@ -86,26 +95,25 @@ function App() {
   console.log(preview, "preview");
   return (
     <>
-      <div className="flex self-center justify-center w-full h-screen bg-gray-900">
-        <div className="hidden">
-          <input
-            onChange={uploadImage}
-            id="image"
-            placeholder="Click to upload image"
-            accept="image*/"
-            type="file"
-          />
-          {selectedFile && <img src={preview} alt="preview" />}
-        </div> 
+    <div className="flex flex-row w-full h-screen">
+      <div className="w-1/5 p-2 flex flex-col items-center  bg-fuchsia-300">
+      <h1 className="w-full mx-4 p-1 text-xl bg-fuchsia-400 flex">Recent Chat</h1>    
+      </div>
+      <div className="flex  flex-col   items-center  w-full h-screen bg-gray-950 ">
+         <div className="text-white flex justify-center my-auto w-full"> 
+         <div className="text-6xl font-bold bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+     Welcome , Let's Chat
+    </div>
+         </div>
          <div className="flex flex-col w-3/5 h-auto p-1 border-spacing-1 border-slate-200 items-start rounded-3xl fixed bottom-7   bg-slate-700">
-         <div className="w-40 mx-4 h-auto overflow-hidden">{selectedFile && <img className="w-full h-auto object-cover" src={preview} alt="preview" />}</div>
+         <div className="w-40 mx-4 h-auto overflow-hidden">{selectedFile && <img className="w-full shadow-md shadow-black h-auto object-cover" src={preview} alt="preview" />}</div>
          <div className="flex flex-col w-full h-28 px-1 border-spacing-1 border-slate-200 items-center rounded-3xl     bg-slate-700 justify-center">
           <div className="w-full ">
             <input
               onChange={(e) => {
                 setSupriseVal(e.target.value);
               }}
-              className="border-2 w-full h-14 my-1 rounded-2xl py-1 placeholder:text-slate-500 bg-slate-300 border-stone-500"
+              className="border-2 p-2 w-full h-14 my-1 rounded-2xl py-1 placeholder:text-slate-500 bg-slate-300 border-stone-500"
               type="text"
               value={supriseVal}
               placeholder="What do you want to know about the image ..."
@@ -114,6 +122,10 @@ function App() {
           <div className="flex flex-col w-full  ">
             <div className="flex w-full justify-between ">
               <div className="flex flex-row">
+                <button className="p-2  rounded-xl mx-1 text-slate-300 bg-slate-600 hover:bg-slate-500 cursor-pointer" onClick={handleOnRecord}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+</svg>
+</button>
                 <input
                   onChange={uploadImage}
                   id="image"
@@ -160,6 +172,8 @@ function App() {
          </div>
        
       </div>
+    </div>
+     
     </>
   );
 }
